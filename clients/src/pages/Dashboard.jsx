d64229c69d800d6ee13c4f023e2d6358c3d788c2
Satyam Stellar <satyam321tomar@gmail.com>
2025-02-12 21:50:43 +0530

    start working on dashboard

--- clients/src/pages/Dashboard.jsx
+++ clients/src/pages/Dashboard.jsx
@@ -1,7 +1,55 @@
+import {
+  MdAdminPanelSettings,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper, FaUsers } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+
+import moment from "moment";
+import { summary } from "../assets/data.js"
 
 const Dashboard = () => {
+
+  const totals = summary.tasks
+
+  const stats = [
+    {
+      _id: "1",
+      label: "TOTAL TASK",
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: "bg-[#1d4ed8]",
+    },
+    {
+      _id: "2",
+      label: "COMPLTED TASK",
+      total: totals["completed"] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: "bg-[#0f766e]",
+    },
+    {
+      _id: "3",
+      label: "TASK IN PROGRESS ",
+      total: totals["in progress"] || 0,
+      icon: <LuClipboardEdit />,
+      bg: "bg-[#f59e0b]",
+    },
+    {
+      _id: "4",
+      label: "TODOS",
+      total: totals["todo"],
+      icon: <FaArrowsToDot />,
+      bg: "bg-[#be185d]" || 0,
+    },
+  ];
+
   return (
-    <div className="">Dashboard</div>
+    <div className="w-full py-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-5"></div>
+    </div>
   )
 }
 
