16b06fd37878122778cdec2ba8296e277db6a514
Satyam Stellar <satyam321tomar@gmail.com>
2025-02-22 22:50:34 +0530

    REMOVE: package-lock.json & add cards in dasboad

--- clients/src/pages/Dashboard.jsx
+++ clients/src/pages/Dashboard.jsx
@@ -10,6 +10,7 @@ import { FaArrowsToDot } from "react-icons/fa6";
 
 import moment from "moment";
 import { summary } from "../assets/data.js"
+import Chart from "../components/Chart.jsx";
 
 const Dashboard = () => {
 
@@ -46,9 +47,38 @@ const Dashboard = () => {
     },
   ];
 
+  const Card = ({ bg, icon, count, label }) => {
+    return (
+      <div className="w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-center">
+        <div className=" flex flex-1 h-full flex-col justify-between">
+          <p className="text-base text-gray-700">{label}</p>
+          <span className="text-2xl font-semibold">{count}</span>
+          <span className="text-sm text-gray-600">{"last month 11"}</span>
+        </div>
+        <div className={`w-10 h-10 flex rounded-full items-center justify-center text-white ${bg}`}>
+          {icon}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full py-4 ">
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-5"></div>
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-5">
+        {stats.map(({ icon, bg, total, label }, index) => (
+          <Card
+            key={index}
+            bg={bg}
+            icon={icon}
+            count={total}
+            label={label}
+          />
+        ))}
+      </div>
+      <div className="bg-white w-full my-16 p-4 rounded shadow">
+        <h4 className="text-2xl text-gray-700 font-semibold">Chart by Priority</h4>
+        <Chart />
+      </div>
     </div>
   )
 }
